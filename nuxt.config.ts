1e31ac0679311a4e2bb29d58f3a0fd97461be7e9
Tran Khac Binh Duong <tran.khac.binh.duong@azoom.jp>
2024-10-31 13:20:25 +0700

    :art: Setup nuxt primevue UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+import Aura from '@primevue/themes/aura';
+import { definePreset } from '@primevue/themes';
+
+const NoirPreset = useCustomThemePreset()
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@primevue/nuxt-module'],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4
@@ -14,5 +19,69 @@ export default defineNuxtConfig({
       }
     }
   },
-  css: ['~/assets/styles/app.css'],
+  primevue: {
+    autoImport: false,
+    options: {
+      prefix: 'p',
+      ripple: true,
+      theme: {
+        preset: NoirPreset
+      }
+    },
+    components: {
+      prefix: 'p'
+    }
+  },
+  css: ['~/assets/styles/app.css', 'primeicons/primeicons.css'],
 })
+
+function useCustomThemePreset() {
+  // https://nuxt.com/docs/api/configuration/nuxt-config
+  return definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{zinc.50}',
+            100: '{zinc.100}',
+            200: '{zinc.200}',
+            300: '{zinc.300}',
+            400: '{zinc.400}',
+            500: '{zinc.500}',
+            600: '{zinc.600}',
+            700: '{zinc.700}',
+            800: '{zinc.800}',
+            900: '{zinc.900}',
+            950: '{zinc.950}'
+        },
+        colorScheme: {
+            light: {
+                primary: {
+                    color: '{zinc.950}',
+                    inverseColor: '#ffffff',
+                    hoverColor: '{zinc.900}',
+                    activeColor: '{zinc.800}'
+                },
+                highlight: {
+                    background: '{zinc.950}',
+                    focusBackground: '{zinc.700}',
+                    color: '#ffffff',
+                    focusColor: '#ffffff'
+                }
+            },
+            dark: {
+                primary: {
+                    color: '{zinc.50}',
+                    inverseColor: '{zinc.950}',
+                    hoverColor: '{zinc.100}',
+                    activeColor: '{zinc.200}'
+                },
+                highlight: {
+                    background: 'rgba(250, 250, 250, .16)',
+                    focusBackground: 'rgba(250, 250, 250, .24)',
+                    color: 'rgba(255,255,255,.87)',
+                    focusColor: 'rgba(255,255,255,.87)'
+                }
+            }
+        }
+    }
+  });
+}
