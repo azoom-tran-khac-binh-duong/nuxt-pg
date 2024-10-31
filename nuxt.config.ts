18f08860268ca5154b4ff2e2f7a212f526ce5a3a
Tran Khac Binh Duong <tran.khac.binh.duong@azoom.jp>
2024-10-31 12:56:02 +0700

    :rotating_light: Add nuxt eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
   future: {
-    compatibilityVersion: 4,
+    compatibilityVersion: 4
   },
+  compatibilityDate: '2024-04-03',
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
